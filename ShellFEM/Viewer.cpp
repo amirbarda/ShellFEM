160a119ecb2cc4c0b09fe0ac42d8deac6064b69e
michaelsehaik <michaelsehaik@gmail.com>
2021-03-03 19:08:40 +0200

    added missing eigen include files, and updated gitignore.

--- ShellFEM/Viewer.cpp
+++ ShellFEM/Viewer.cpp
@@ -5,6 +5,7 @@
 void Viewer::startView(Eigen::MatrixXd const &V, Eigen::MatrixXi const &F, VectorXd &vonMisesStress) {
 	MatrixXd C;
 
+	viewer.core().lighting_factor = 0;
 	viewer.data().set_mesh(V, F);
 	viewer.data().set_face_based(true);
 	viewer.core().set_rotation_type(igl::opengl::ViewerCore::ROTATION_TYPE_TRACKBALL);
